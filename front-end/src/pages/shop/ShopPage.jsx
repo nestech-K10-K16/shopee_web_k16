c4ce127b13a99fe9c82974153a72242441780334
Hao <kenaru3s@gmail.com>
2023-07-22 19:20:43 +0700

    nguyengiahoanhao/front-end/my-account/create-ui - update-ui

--- front-end/src/pages/shop/ShopPage.jsx
+++ front-end/src/pages/shop/ShopPage.jsx
@@ -12,45 +12,39 @@ const ShopPage = () => {
     <>
       <DefaultLayout>
         <div className="Shoppe__shopPage">
-          <h1>Shop The Latest</h1>
           <div className="Shoppe__shopPage-container">
-            <div className="Shoppe__shopPage-text"></div>
-              <div className="Shoppe__shopPage-search">
-                <input type="text" placeholder="Search" />
-                <Link to="/search">
-                  <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
-                </Link>
-              </div>
+            <h1>Shop The Latest</h1>
+            <div className="Shoppe__shopPage-search">
+              <input type="text" placeholder="Search" />
+              <Link to="/search">
+                <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+              </Link>
             </div>
-            <div className="Shoppe__shopPage_home-container"> 
-              <div className="Shoppe__shopPage_home-container_group">
-                <Jewelry
-                  imgUrl={home01}
-                  text="Lira Earrings"
-                  dollar="$ 20,00"
-                />
-                <Jewelry imgUrl={home02} text="Hal Earrings" dollar="$ 25,00" />
-                <Jewelry
-                  imgUrl={home03}
-                  text="Kaede Hair Pin Set Of 3 "
-                  dollar="$ 30,00"
-                />
-                <Jewelry
-                  imgUrl={home04}
-                  text="Hair Pin Set of 3"
-                  dollar="$ 30,00"
-                />
-                <Jewelry
-                  imgUrl={home05}
-                  text="Plaine Necklace"
-                  dollar="$ 19,00"
-                />
-                <Jewelry
-                  imgUrl={home06}
-                  text="Yuki Hair Pin Set of 3"
-                  dollar="$ 29,00"
-                />
-              </div>
+          </div>
+          <div className="Shoppe__shopPage_home-container">
+            <div className="Shoppe__shopPage_home-container_group">
+              <Jewelry imgUrl={home01} text="Lira Earrings" dollar="$ 20,00" />
+              <Jewelry imgUrl={home02} text="Hal Earrings" dollar="$ 25,00" />
+              <Jewelry
+                imgUrl={home03}
+                text="Kaede Hair Pin Set Of 3 "
+                dollar="$ 30,00"
+              />
+              <Jewelry
+                imgUrl={home04}
+                text="Hair Pin Set of 3"
+                dollar="$ 30,00"
+              />
+              <Jewelry
+                imgUrl={home05}
+                text="Plaine Necklace"
+                dollar="$ 19,00"
+              />
+              <Jewelry
+                imgUrl={home06}
+                text="Yuki Hair Pin Set of 3"
+                dollar="$ 29,00"
+              />
             </div>
           </div>
         </div>
